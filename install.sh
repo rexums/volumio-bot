bbe11ffe197ec542ca2e78ed76e2c1ab32bc437c
rexjohannes <53578137+rexjohannes@users.noreply.github.com>
2020-12-06 16:01:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt install python3-pip python3 -y
-sudo apt install git screen -y
-sudo apt install libopus-dev -y
+sudo su
+apt update -y
+apt upgrade -y
+apt install python3-pip python3 -y
+apt install git screen -y
+apt install libopus-dev -y
 git clone https://github.com/rexjohannes/volumio-bot.git
 cd volumio-bot
-sudo python3 -m pip install --upgrade -r requirements.txt
+python3 -m pip install --upgrade -r requirements.txt
 chmod +x run.sh
 nano ./config/token.txt
 bash run.sh
