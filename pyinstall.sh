75a8fd3ecd1128e1564902a8302bdb6c757c7127
rexjohannes <53578137+rexjohannes@users.noreply.github.com>
2020-12-07 17:44:42 +0100

    Update pyinstall.sh

--- pyinstall.sh
+++ pyinstall.sh
@@ -1,11 +1,11 @@
 sudo apt-get update -y
 sudo apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev -y
-wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tar.xz
-tar xf Python-3.7.4.tar.xz
-cd Python-3.7.4
+wget https://www.python.org/ftp/python/3.7.4/Python-3.6.2.tar.xz
+tar xf Python-3.6.2.tar.xz
+cd Python-3.6.2
 ./configure
 make -j 4
 sudo make altinstall
 cd ..
 sudo rm -r Python-3.4.0
-rm Python-3.7.4.tar.xz
+rm Python-3.6.2.tar.xz
